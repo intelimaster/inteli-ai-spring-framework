spring-projects/spring-ai

Add VertexAI Embedding Model support
 - add new spring-ai-vertex-ai-embedding project.
 - add VertexAiTextEmbeddingModel and VertexAiMultimodalEmbeddingMode with related options configuration classes.
 - add ITs
 - add auto-configuraiton and boot starters.
 - register to BOM.
 - add documentation.
 - add multimodal embedding documentation
 - extend the Embedding metdata so that it can keep references to the source document's data, Id, mediatype

 Resolves #1013
 Related to #1009

models/spring-ai-bedrock/src/main/java/org/springframework/ai/bedrock/cohere/api/CohereChatBedrockApi.java
@@ -30,7 +30,7 @@ import software.amazon.awssdk.regions.Region;
 import org.springframework.ai.bedrock.api.AbstractBedrockApi;
 import org.springframework.ai.bedrock.cohere.api.CohereChatBedrockApi.CohereChatRequest;
 import org.springframework.ai.bedrock.cohere.api.CohereChatBedrockApi.CohereChatResponse;
-import org.springframework.ai.model.ModelDescription;
+import org.springframework.ai.model.ChatModelDescription;
 import org.springframework.util.Assert;
 
 /**
@@ -367,7 +367,7 @@ public class CohereChatBedrockApi extends
 	/**
 	 * Cohere models version.
 	 */
-	public enum CohereChatModel implements ModelDescription {
+	public enum CohereChatModel implements ChatModelDescription {
 
 		/**
 		 * cohere.command-light-text-v14
@@ -393,7 +393,7 @@ public class CohereChatBedrockApi extends
 		}
 
 		@Override
-		public String getModelName() {
+		public String getTextName() {
 			return this.id;
 		}
 	}

models/spring-ai-bedrock/src/main/java/org/springframework/ai/bedrock/jurassic2/api/Ai21Jurassic2ChatBedrockApi.java
@@ -27,7 +27,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.ai.bedrock.api.AbstractBedrockApi;
 import org.springframework.ai.bedrock.jurassic2.api.Ai21Jurassic2ChatBedrockApi.Ai21Jurassic2ChatRequest;
 import org.springframework.ai.bedrock.jurassic2.api.Ai21Jurassic2ChatBedrockApi.Ai21Jurassic2ChatResponse;
-import org.springframework.ai.model.ModelDescription;
+import org.springframework.ai.model.ChatModelDescription;
 
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -372,7 +372,7 @@ public class Ai21Jurassic2ChatBedrockApi extends
 	/**
 	 * Ai21 Jurassic2 models version.
 	 */
-	public enum Ai21Jurassic2ChatModel implements ModelDescription {
+	public enum Ai21Jurassic2ChatModel implements ChatModelDescription {
 
 		/**
 		 * ai21.j2-mid-v1
@@ -398,7 +398,7 @@ public class Ai21Jurassic2ChatBedrockApi extends
 		}
 
 		@Override
-		public String getModelName() {
+		public String getTextName() {
 			return this.id;
 		}
 	}

models/spring-ai-minimax/src/main/java/org/springframework/ai/minimax/api/MiniMaxApi.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
-import org.springframework.ai.model.ModelDescription;
+import org.springframework.ai.model.ChatModelDescription;
 import org.springframework.ai.model.ModelOptionsUtils;
 import org.springframework.ai.retry.RetryUtils;
 import org.springframework.ai.util.api.ApiUtils;
@@ -113,7 +113,7 @@ public class MiniMaxApi {
 	 * MiniMax Chat Completion Models:
 	 * <a href="https://www.minimaxi.com/document/algorithm-concept">MiniMax Model</a>.
 	 */
-	public enum ChatModel implements ModelDescription {
+	public enum ChatModel implements ChatModelDescription {
 		ABAB_6_5_Chat("abab6.5-chat"),
 		ABAB_6_5_S_Chat("abab6.5s-chat"),
 		ABAB_6_5_T_Chat("abab6.5t-chat"),
@@ -135,7 +135,7 @@ public class MiniMaxApi {
 		}
 
 		@Override
-		public String getModelName() {
+		public String getTextName() {
 			return this.value;
 		}
 	}

models/spring-ai-mistral-ai/src/test/java/org/springframework/ai/mistralai/MistralAiChatClientIT.java
@@ -251,7 +251,7 @@ class MistralAiChatClientIT {
 
 	@Test
 	void validateCallResponseMetadata() {
-		String model = MistralAiApi.ChatModel.OPEN_MISTRAL_7B.getModelName();
+		String model = MistralAiApi.ChatModel.OPEN_MISTRAL_7B.getTextName();
 		// @formatter:off
 		ChatResponse response = ChatClient.create(chatModel).prompt()
 				.options(MistralAiChatOptions.builder().withModel(model).build())

models/spring-ai-openai/src/test/java/org/springframework/ai/openai/chat/OpenAiPaymentTransactionIT.java
@@ -198,7 +198,7 @@ public class OpenAiPaymentTransactionIT {
 		public OpenAiChatModel openAiClient(OpenAiApi openAiApi, FunctionCallbackContext functionCallbackContext) {
 			return new OpenAiChatModel(openAiApi,
 					OpenAiChatOptions.builder()
-						.withModel(ChatModel.GPT_4_TURBO.getModelName())
+						.withModel(ChatModel.GPT_4_TURBO.getTextName())
 						.withTemperature(0.1f)
 						.build(),
 					functionCallbackContext, RetryUtils.DEFAULT_RETRY_TEMPLATE);

models/spring-ai-vertex-ai-embedding/src/main/java/org/springframework/ai/vertexai/embedding/text/VertexAiTextEmbeddingModelName.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright 2024 - 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.vertexai.embedding.text;
+
+import org.springframework.ai.model.EmbeddingModelDescription;
+
+/**
+ * VertexAI Embedding Models: - <a href=
+ * "https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/text-embeddings-api">Text
+ * embeddings</a> - <a href=
+ * "https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/multimodal-embeddings-api">Multimodal
+ * embeddings</a>
+ *
+ * @author Christian Tzolov
+ * @since 1.0.0
+ */
+public enum VertexAiTextEmbeddingModelName implements EmbeddingModelDescription {
+
+	/**
+	 * English model. Expires on May 14, 2025.
+	 */
+	TEXT_EMBEDDING_004("text-embedding-004", "004", 768, "English text model"),
+
+	/**
+	 * Multilingual model. Expires on May 14, 2025.
+	 */
+	TEXT_MULTILINGUAL_EMBEDDING_002("text-multilingual-embedding-002", "002", 768, "Multilingual text model");
+
+	private final String modelVersion;
+
+	private final String modelName;
+
+	private final String description;
+
+	private final int dimensions;
+
+	VertexAiTextEmbeddingModelName(String value, String modelVersion, int dimensions, String description) {
+		this.modelName = value;
+		this.modelVersion = modelVersion;
+		this.dimensions = dimensions;
+		this.description = description;
+	}
+
+	@Override
+	public String getTextName() {
+		return this.modelName;
+	}
+
+	@Override
+	public String getVersion() {
+		return this.modelVersion;
+	}
+
+	@Override
+	public int getDimensions() {
+		return this.dimensions;
+	}
+
+	@Override
+	public String getDescription() {
+		return this.description;
+	}
+
+}

intelimaster/inteli-ai-spring-framework

models/spring-ai-bedrock/src/main/java/org/springframework/ai/bedrock/cohere/api/CohereChatBedrockApi.java
@@ -30,7 +30,7 @@ import software.amazon.awssdk.regions.Region;
 import org.springframework.ai.bedrock.api.AbstractBedrockApi;
 import org.springframework.ai.bedrock.cohere.api.CohereChatBedrockApi.CohereChatRequest;
 import org.springframework.ai.bedrock.cohere.api.CohereChatBedrockApi.CohereChatResponse;
-import org.springframework.ai.model.ModelDescription;
+import org.springframework.ai.model.ChatModelDescription;
 import org.springframework.util.Assert;
 
 /**
@@ -367,7 +367,7 @@ public class CohereChatBedrockApi extends
 	/**
 	 * Cohere models version.
 	 */
-	public enum CohereChatModel implements ModelDescription {
+	public enum CohereChatModel implements ChatModelDescription {
 
 		/**
 		 * cohere.command-light-text-v14
@@ -393,7 +393,7 @@ public class CohereChatBedrockApi extends
 		}
 
 		@Override
-		public String getModelName() {
+		public String getName() {
 			return this.id;
 		}
 	}

models/spring-ai-bedrock/src/main/java/org/springframework/ai/bedrock/jurassic2/api/Ai21Jurassic2ChatBedrockApi.java
@@ -27,7 +27,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.ai.bedrock.api.AbstractBedrockApi;
 import org.springframework.ai.bedrock.jurassic2.api.Ai21Jurassic2ChatBedrockApi.Ai21Jurassic2ChatRequest;
 import org.springframework.ai.bedrock.jurassic2.api.Ai21Jurassic2ChatBedrockApi.Ai21Jurassic2ChatResponse;
-import org.springframework.ai.model.ModelDescription;
+import org.springframework.ai.model.ChatModelDescription;
 
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
@@ -372,7 +372,7 @@ public class Ai21Jurassic2ChatBedrockApi extends
 	/**
 	 * Ai21 Jurassic2 models version.
 	 */
-	public enum Ai21Jurassic2ChatModel implements ModelDescription {
+	public enum Ai21Jurassic2ChatModel implements ChatModelDescription {
 
 		/**
 		 * ai21.j2-mid-v1
@@ -398,7 +398,7 @@ public class Ai21Jurassic2ChatBedrockApi extends
 		}
 
 		@Override
-		public String getModelName() {
+		public String getName() {
 			return this.id;
 		}
 	}

models/spring-ai-minimax/src/main/java/org/springframework/ai/minimax/api/MiniMaxApi.java
@@ -19,7 +19,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
-import org.springframework.ai.model.ModelDescription;
+import org.springframework.ai.model.ChatModelDescription;
 import org.springframework.ai.model.ModelOptionsUtils;
 import org.springframework.ai.retry.RetryUtils;
 import org.springframework.ai.util.api.ApiUtils;
@@ -113,7 +113,7 @@ public class MiniMaxApi {
 	 * MiniMax Chat Completion Models:
 	 * <a href="https://www.minimaxi.com/document/algorithm-concept">MiniMax Model</a>.
 	 */
-	public enum ChatModel implements ModelDescription {
+	public enum ChatModel implements ChatModelDescription {
 		ABAB_6_5_Chat("abab6.5-chat"),
 		ABAB_6_5_S_Chat("abab6.5s-chat"),
 		ABAB_6_5_T_Chat("abab6.5t-chat"),
@@ -135,7 +135,7 @@ public class MiniMaxApi {
 		}
 
 		@Override
-		public String getModelName() {
+		public String getName() {
 			return this.value;
 		}
 	}

models/spring-ai-mistral-ai/src/test/java/org/springframework/ai/mistralai/MistralAiChatClientIT.java
@@ -251,7 +251,7 @@ class MistralAiChatClientIT {
 
 	@Test
 	void validateCallResponseMetadata() {
-		String model = MistralAiApi.ChatModel.OPEN_MISTRAL_7B.getModelName();
+		String model = MistralAiApi.ChatModel.OPEN_MISTRAL_7B.getName();
 		// @formatter:off
 		ChatResponse response = ChatClient.create(chatModel).prompt()
 				.options(MistralAiChatOptions.builder().withModel(model).build())

models/spring-ai-openai/src/test/java/org/springframework/ai/openai/chat/OpenAiPaymentTransactionIT.java
@@ -198,7 +198,7 @@ public class OpenAiPaymentTransactionIT {
 		public OpenAiChatModel openAiClient(OpenAiApi openAiApi, FunctionCallbackContext functionCallbackContext) {
 			return new OpenAiChatModel(openAiApi,
 					OpenAiChatOptions.builder()
-						.withModel(ChatModel.GPT_4_TURBO.getModelName())
+						.withModel(ChatModel.GPT_4_TURBO.getName())
 						.withTemperature(0.1f)
 						.build(),
 					functionCallbackContext, RetryUtils.DEFAULT_RETRY_TEMPLATE);

models/spring-ai-vertex-ai-embedding/src/main/java/org/springframework/ai/vertexai/embedding/text/VertexAiTextEmbeddingModelName.java
@@ -1,0 +1,77 @@
+/*
+ * Copyright 2024 - 2024 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.vertexai.embedding.text;
+
+import org.springframework.ai.model.EmbeddingModelDescription;
+
+/**
+ * VertexAI Embedding Models: - <a href=
+ * "https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/text-embeddings-api">Text
+ * embeddings</a> - <a href=
+ * "https://cloud.google.com/vertex-ai/generative-ai/docs/model-reference/multimodal-embeddings-api">Multimodal
+ * embeddings</a>
+ *
+ * @author Christian Tzolov
+ * @since 1.0.0
+ */
+public enum VertexAiTextEmbeddingModelName implements EmbeddingModelDescription {
+
+	/**
+	 * English model. Expires on May 14, 2025.
+	 */
+	TEXT_EMBEDDING_004("text-embedding-004", "004", 768, "English text model"),
+
+	/**
+	 * Multilingual model. Expires on May 14, 2025.
+	 */
+	TEXT_MULTILINGUAL_EMBEDDING_002("text-multilingual-embedding-002", "002", 768, "Multilingual text model");
+
+	private final String modelVersion;
+
+	private final String modelName;
+
+	private final String description;
+
+	private final int dimensions;
+
+	VertexAiTextEmbeddingModelName(String value, String modelVersion, int dimensions, String description) {
+		this.modelName = value;
+		this.modelVersion = modelVersion;
+		this.dimensions = dimensions;
+		this.description = description;
+	}
+
+	@Override
+	public String getName() {
+		return this.modelName;
+	}
+
+	@Override
+	public String getVersion() {
+		return this.modelVersion;
+	}
+
+	@Override
+	public int getDimensions() {
+		return this.dimensions;
+	}
+
+	@Override
+	public String getDescription() {
+		return this.description;
+	}
+
+}