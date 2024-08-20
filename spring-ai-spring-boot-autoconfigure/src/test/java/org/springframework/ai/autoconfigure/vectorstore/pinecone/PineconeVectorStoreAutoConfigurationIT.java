spring-projects/spring-ai

Add observability support to existing vector stores
 Add observability support to:
 - Cassandra
 - Chroma
 - Elasticsearch
 - Milvus
 - Neo4j
 - OpenSearch
 - Qdrant
 - Redis
 - Typesense
 - Weaviate
 - Pinecone
 - Oracle
 - Gemifire
 - MongoDB
 - HanaDB

 Add autoconfiguration obsrvability for the above vector stores.
 Add integration tests for all vector stores.

spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/vectorstore/pinecone/PineconeVectorStoreAutoConfigurationIT.java
@@ -17,6 +17,7 @@ package org.springframework.ai.autoconfigure.vectorstore.pinecone;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.ai.autoconfigure.vectorstore.observation.ObservationTestUtil.assertObservationRegistry;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -31,14 +32,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.observation.conventions.VectorStoreProvider;
 import org.springframework.ai.transformers.TransformersEmbeddingModel;
 import org.springframework.ai.vectorstore.PineconeVectorStore;
 import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationContext;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.DefaultResourceLoader;
+
+import io.micrometer.observation.tck.TestObservationRegistry;
 
 /**
  * @author Christian Tzolov
@@ -85,12 +90,17 @@ public class PineconeVectorStoreAutoConfigurationIT {
 		contextRunner.run(context -> {
 
 			PineconeVectorStore vectorStore = context.getBean(PineconeVectorStore.class);
+			TestObservationRegistry observationRegistry = context.getBean(TestObservationRegistry.class);
 
 			vectorStore.add(documents);
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.PINECONE_VECTOR_STORE,
+					VectorStoreObservationContext.Operation.ADD);
 
 			Awaitility.await().until(() -> {
 				return vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(1));
 			}, hasSize(1));
+			observationRegistry.clear();
 
 			List<Document> results = vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(1));
 
@@ -102,8 +112,16 @@ public class PineconeVectorStoreAutoConfigurationIT {
 			assertThat(resultDoc.getMetadata()).hasSize(2);
 			assertThat(resultDoc.getMetadata()).containsKeys("spring", "customDistanceField");
 
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.PINECONE_VECTOR_STORE,
+					VectorStoreObservationContext.Operation.QUERY);
+			observationRegistry.clear();
+
 			// Remove all documents from the store
 			vectorStore.delete(documents.stream().map(doc -> doc.getId()).toList());
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.PINECONE_VECTOR_STORE,
+					VectorStoreObservationContext.Operation.DELETE);
+			observationRegistry.clear();
 
 			Awaitility.await().until(() -> {
 				return vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(1));
@@ -113,6 +131,11 @@ public class PineconeVectorStoreAutoConfigurationIT {
 
 	@Configuration(proxyBeanMethods = false)
 	static class Config {
+
+		@Bean
+		public TestObservationRegistry observationRegistry() {
+			return TestObservationRegistry.create();
+		}
 
 		@Bean
 		public EmbeddingModel embeddingModel() {

spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/vectorstore/weaviate/WeaviateVectorStoreAutoConfigurationIT.java
@@ -24,17 +24,22 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.observation.conventions.VectorStoreProvider;
 import org.springframework.ai.transformers.TransformersEmbeddingModel;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.WeaviateVectorStore.WeaviateVectorStoreConfig.MetadataField;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationContext;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.testcontainers.weaviate.WeaviateContainer;
 
+import io.micrometer.observation.tck.TestObservationRegistry;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.ai.autoconfigure.vectorstore.observation.ObservationTestUtil.assertObservationRegistry;
 
 /**
  * @author Christian Tzolov
@@ -73,12 +78,18 @@ public class WeaviateVectorStoreAutoConfigurationIT {
 
 			VectorStore vectorStore = context.getBean(VectorStore.class);
 
+			TestObservationRegistry observationRegistry = context.getBean(TestObservationRegistry.class);
+
 			var bgDocument = new Document("The World is Big and Salvation Lurks Around the Corner",
 					Map.of("country", "Bulgaria", "price", 3.14, "active", true, "year", 2020));
 			var nlDocument = new Document("The World is Big and Salvation Lurks Around the Corner",
 					Map.of("country", "Netherlands", "price", 1.57, "active", false, "year", 2023));
 
 			vectorStore.add(List.of(bgDocument, nlDocument));
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.WEAVIATE_VECTOR_STORE,
+					VectorStoreObservationContext.Operation.ADD);
+			observationRegistry.clear();
 
 			var request = SearchRequest.query("The World").withTopK(5);
 
@@ -89,6 +100,9 @@ public class WeaviateVectorStoreAutoConfigurationIT {
 				.similaritySearch(request.withSimilarityThresholdAll().withFilterExpression("country == 'Bulgaria'"));
 			assertThat(results).hasSize(1);
 			assertThat(results.get(0).getId()).isEqualTo(bgDocument.getId());
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.WEAVIATE_VECTOR_STORE,
+					VectorStoreObservationContext.Operation.QUERY);
 
 			results = vectorStore.similaritySearch(
 					request.withSimilarityThresholdAll().withFilterExpression("country == 'Netherlands'"));
@@ -109,13 +123,23 @@ public class WeaviateVectorStoreAutoConfigurationIT {
 			assertThat(results).hasSize(1);
 			assertThat(results.get(0).getId()).isEqualTo(nlDocument.getId());
 
+			observationRegistry.clear();
+
 			// Remove all documents from the store
 			vectorStore.delete(List.of(bgDocument, nlDocument).stream().map(doc -> doc.getId()).toList());
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.WEAVIATE_VECTOR_STORE,
+					VectorStoreObservationContext.Operation.DELETE);
 		});
 	}
 
 	@Configuration(proxyBeanMethods = false)
 	static class Config {
+
+		@Bean
+		public TestObservationRegistry observationRegistry() {
+			return TestObservationRegistry.create();
+		}
 
 		@Bean
 		public EmbeddingModel embeddingModel() {

vector-stores/spring-ai-hanadb-store/src/main/java/org/springframework/ai/vectorstore/HanaCloudVectorStore.java
@@ -16,11 +16,20 @@
 package org.springframework.ai.vectorstore;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.micrometer.observation.ObservationRegistry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.model.EmbeddingUtils;
+import org.springframework.ai.observation.conventions.VectorStoreProvider;
+import org.springframework.ai.observation.conventions.VectorStoreSimilarityMetric;
+import org.springframework.ai.vectorstore.observation.AbstractObservationVectorStore;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationContext;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationContext.Builder;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationConvention;
 
 import java.util.Collections;
 import java.util.List;
@@ -54,12 +63,13 @@ import java.util.stream.Collectors;
  * generated by <code>org.springframework.ai.embedding.EmbeddingModel</code>
  *
  * @author Rahul Mittal
+ * @author Christian Tzolov
  * @see <a href=
  * "https://help.sap.com/docs/hana-cloud-database/sap-hana-cloud-sap-hana-database-vector-engine-guide/introduction">SAP
  * HANA Database Vector Engine Guide</a>
  * @since 1.0.0
  */
-public class HanaCloudVectorStore implements VectorStore {
+public class HanaCloudVectorStore extends AbstractObservationVectorStore {
 
 	private static final Logger logger = LoggerFactory.getLogger(HanaCloudVectorStore.class);
 
@@ -71,13 +81,23 @@ public class HanaCloudVectorStore implements VectorStore {
 
 	public HanaCloudVectorStore(HanaVectorRepository<? extends HanaVectorEntity> repository,
 			EmbeddingModel embeddingModel, HanaCloudVectorStoreConfig config) {
+
+		this(repository, embeddingModel, config, ObservationRegistry.NOOP, null);
+	}
+
+	public HanaCloudVectorStore(HanaVectorRepository<? extends HanaVectorEntity> repository,
+			EmbeddingModel embeddingModel, HanaCloudVectorStoreConfig config, ObservationRegistry observationRegistry,
+			VectorStoreObservationConvention customObservationConvention) {
+
+		super(observationRegistry, customObservationConvention);
+
 		this.repository = repository;
 		this.embeddingModel = embeddingModel;
 		this.config = config;
 	}
 
 	@Override
-	public void add(List<Document> documents) {
+	public void doAdd(List<Document> documents) {
 		int count = 1;
 		for (Document document : documents) {
 			logger.info("[{}/{}] Calling EmbeddingModel for document id = {}", count++, documents.size(),
@@ -90,7 +110,7 @@ public class HanaCloudVectorStore implements VectorStore {
 	}
 
 	@Override
-	public Optional<Boolean> delete(List<String> idList) {
+	public Optional<Boolean> doDelete(List<String> idList) {
 		int deleteCount = repository.deleteEmbeddingsById(config.getTableName(), idList);
 		logger.info("{} embeddings deleted", deleteCount);
 		return Optional.of(deleteCount == idList.size());
@@ -108,7 +128,7 @@ public class HanaCloudVectorStore implements VectorStore {
 	}
 
 	@Override
-	public List<Document> similaritySearch(SearchRequest request) {
+	public List<Document> doSimilaritySearch(SearchRequest request) {
 		if (request.hasFilterExpression()) {
 			throw new UnsupportedOperationException(
 					"SAPHanaVectorEngine does not support metadata filter expressions yet.");
@@ -142,6 +162,15 @@ public class HanaCloudVectorStore implements VectorStore {
 			.stream()
 			.map(String::valueOf)
 			.collect(Collectors.joining(", ")) + "]";
+	}
+
+	@Override
+	public Builder createObservationContextBuilder(String operationName) {
+
+		return VectorStoreObservationContext.builder(VectorStoreProvider.HANA_VECTOR_STORE.value(), operationName)
+			.withDimensions(this.embeddingModel.dimensions())
+			.withCollectionName(this.config.getTableName())
+			.withSimilarityMetric(VectorStoreSimilarityMetric.COSINE.value());
 	}
 
 }

intelimaster/inteli-ai-spring-framework

spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/vectorstore/pinecone/PineconeVectorStoreAutoConfigurationIT.java
@@ -17,6 +17,7 @@ package org.springframework.ai.autoconfigure.vectorstore.pinecone;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.ai.autoconfigure.vectorstore.observation.ObservationTestUtil.assertObservationRegistry;
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -31,14 +32,18 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.observation.conventions.VectorStoreProvider;
 import org.springframework.ai.transformers.TransformersEmbeddingModel;
 import org.springframework.ai.vectorstore.PineconeVectorStore;
 import org.springframework.ai.vectorstore.SearchRequest;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationContext;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.DefaultResourceLoader;
+
+import io.micrometer.observation.tck.TestObservationRegistry;
 
 /**
  * @author Christian Tzolov
@@ -85,12 +90,17 @@ public class PineconeVectorStoreAutoConfigurationIT {
 		contextRunner.run(context -> {
 
 			PineconeVectorStore vectorStore = context.getBean(PineconeVectorStore.class);
+			TestObservationRegistry observationRegistry = context.getBean(TestObservationRegistry.class);
 
 			vectorStore.add(documents);
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.PINECONE,
+					VectorStoreObservationContext.Operation.ADD);
 
 			Awaitility.await().until(() -> {
 				return vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(1));
 			}, hasSize(1));
+			observationRegistry.clear();
 
 			List<Document> results = vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(1));
 
@@ -102,8 +112,16 @@ public class PineconeVectorStoreAutoConfigurationIT {
 			assertThat(resultDoc.getMetadata()).hasSize(2);
 			assertThat(resultDoc.getMetadata()).containsKeys("spring", "customDistanceField");
 
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.PINECONE,
+					VectorStoreObservationContext.Operation.QUERY);
+			observationRegistry.clear();
+
 			// Remove all documents from the store
 			vectorStore.delete(documents.stream().map(doc -> doc.getId()).toList());
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.PINECONE,
+					VectorStoreObservationContext.Operation.DELETE);
+			observationRegistry.clear();
 
 			Awaitility.await().until(() -> {
 				return vectorStore.similaritySearch(SearchRequest.query("Spring").withTopK(1));
@@ -113,6 +131,11 @@ public class PineconeVectorStoreAutoConfigurationIT {
 
 	@Configuration(proxyBeanMethods = false)
 	static class Config {
+
+		@Bean
+		public TestObservationRegistry observationRegistry() {
+			return TestObservationRegistry.create();
+		}
 
 		@Bean
 		public EmbeddingModel embeddingModel() {

spring-ai-spring-boot-autoconfigure/src/test/java/org/springframework/ai/autoconfigure/vectorstore/weaviate/WeaviateVectorStoreAutoConfigurationIT.java
@@ -24,17 +24,22 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.observation.conventions.VectorStoreProvider;
 import org.springframework.ai.transformers.TransformersEmbeddingModel;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.ai.vectorstore.WeaviateVectorStore.WeaviateVectorStoreConfig.MetadataField;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationContext;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.testcontainers.weaviate.WeaviateContainer;
 
+import io.micrometer.observation.tck.TestObservationRegistry;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.ai.autoconfigure.vectorstore.observation.ObservationTestUtil.assertObservationRegistry;
 
 /**
  * @author Christian Tzolov
@@ -73,12 +78,18 @@ public class WeaviateVectorStoreAutoConfigurationIT {
 
 			VectorStore vectorStore = context.getBean(VectorStore.class);
 
+			TestObservationRegistry observationRegistry = context.getBean(TestObservationRegistry.class);
+
 			var bgDocument = new Document("The World is Big and Salvation Lurks Around the Corner",
 					Map.of("country", "Bulgaria", "price", 3.14, "active", true, "year", 2020));
 			var nlDocument = new Document("The World is Big and Salvation Lurks Around the Corner",
 					Map.of("country", "Netherlands", "price", 1.57, "active", false, "year", 2023));
 
 			vectorStore.add(List.of(bgDocument, nlDocument));
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.WEAVIATE,
+					VectorStoreObservationContext.Operation.ADD);
+			observationRegistry.clear();
 
 			var request = SearchRequest.query("The World").withTopK(5);
 
@@ -89,6 +100,9 @@ public class WeaviateVectorStoreAutoConfigurationIT {
 				.similaritySearch(request.withSimilarityThresholdAll().withFilterExpression("country == 'Bulgaria'"));
 			assertThat(results).hasSize(1);
 			assertThat(results.get(0).getId()).isEqualTo(bgDocument.getId());
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.WEAVIATE,
+					VectorStoreObservationContext.Operation.QUERY);
 
 			results = vectorStore.similaritySearch(
 					request.withSimilarityThresholdAll().withFilterExpression("country == 'Netherlands'"));
@@ -109,13 +123,23 @@ public class WeaviateVectorStoreAutoConfigurationIT {
 			assertThat(results).hasSize(1);
 			assertThat(results.get(0).getId()).isEqualTo(nlDocument.getId());
 
+			observationRegistry.clear();
+
 			// Remove all documents from the store
 			vectorStore.delete(List.of(bgDocument, nlDocument).stream().map(doc -> doc.getId()).toList());
+
+			assertObservationRegistry(observationRegistry, "vector_store", VectorStoreProvider.WEAVIATE,
+					VectorStoreObservationContext.Operation.DELETE);
 		});
 	}
 
 	@Configuration(proxyBeanMethods = false)
 	static class Config {
+
+		@Bean
+		public TestObservationRegistry observationRegistry() {
+			return TestObservationRegistry.create();
+		}
 
 		@Bean
 		public EmbeddingModel embeddingModel() {

vector-stores/spring-ai-hanadb-store/src/main/java/org/springframework/ai/vectorstore/HanaCloudVectorStore.java
@@ -16,11 +16,20 @@
 package org.springframework.ai.vectorstore;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+
+import io.micrometer.observation.ObservationRegistry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.model.EmbeddingUtils;
+import org.springframework.ai.observation.conventions.VectorStoreProvider;
+import org.springframework.ai.observation.conventions.VectorStoreSimilarityMetric;
+import org.springframework.ai.vectorstore.observation.AbstractObservationVectorStore;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationContext;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationContext.Builder;
+import org.springframework.ai.vectorstore.observation.VectorStoreObservationConvention;
 
 import java.util.Collections;
 import java.util.List;
@@ -54,12 +63,13 @@ import java.util.stream.Collectors;
  * generated by <code>org.springframework.ai.embedding.EmbeddingModel</code>
  *
  * @author Rahul Mittal
+ * @author Christian Tzolov
  * @see <a href=
  * "https://help.sap.com/docs/hana-cloud-database/sap-hana-cloud-sap-hana-database-vector-engine-guide/introduction">SAP
  * HANA Database Vector Engine Guide</a>
  * @since 1.0.0
  */
-public class HanaCloudVectorStore implements VectorStore {
+public class HanaCloudVectorStore extends AbstractObservationVectorStore {
 
 	private static final Logger logger = LoggerFactory.getLogger(HanaCloudVectorStore.class);
 
@@ -71,13 +81,23 @@ public class HanaCloudVectorStore implements VectorStore {
 
 	public HanaCloudVectorStore(HanaVectorRepository<? extends HanaVectorEntity> repository,
 			EmbeddingModel embeddingModel, HanaCloudVectorStoreConfig config) {
+
+		this(repository, embeddingModel, config, ObservationRegistry.NOOP, null);
+	}
+
+	public HanaCloudVectorStore(HanaVectorRepository<? extends HanaVectorEntity> repository,
+			EmbeddingModel embeddingModel, HanaCloudVectorStoreConfig config, ObservationRegistry observationRegistry,
+			VectorStoreObservationConvention customObservationConvention) {
+
+		super(observationRegistry, customObservationConvention);
+
 		this.repository = repository;
 		this.embeddingModel = embeddingModel;
 		this.config = config;
 	}
 
 	@Override
-	public void add(List<Document> documents) {
+	public void doAdd(List<Document> documents) {
 		int count = 1;
 		for (Document document : documents) {
 			logger.info("[{}/{}] Calling EmbeddingModel for document id = {}", count++, documents.size(),
@@ -90,7 +110,7 @@ public class HanaCloudVectorStore implements VectorStore {
 	}
 
 	@Override
-	public Optional<Boolean> delete(List<String> idList) {
+	public Optional<Boolean> doDelete(List<String> idList) {
 		int deleteCount = repository.deleteEmbeddingsById(config.getTableName(), idList);
 		logger.info("{} embeddings deleted", deleteCount);
 		return Optional.of(deleteCount == idList.size());
@@ -108,7 +128,7 @@ public class HanaCloudVectorStore implements VectorStore {
 	}
 
 	@Override
-	public List<Document> similaritySearch(SearchRequest request) {
+	public List<Document> doSimilaritySearch(SearchRequest request) {
 		if (request.hasFilterExpression()) {
 			throw new UnsupportedOperationException(
 					"SAPHanaVectorEngine does not support metadata filter expressions yet.");
@@ -142,6 +162,15 @@ public class HanaCloudVectorStore implements VectorStore {
 			.stream()
 			.map(String::valueOf)
 			.collect(Collectors.joining(", ")) + "]";
+	}
+
+	@Override
+	public Builder createObservationContextBuilder(String operationName) {
+
+		return VectorStoreObservationContext.builder(VectorStoreProvider.HANA.value(), operationName)
+			.withDimensions(this.embeddingModel.dimensions())
+			.withCollectionName(this.config.getTableName())
+			.withSimilarityMetric(VectorStoreSimilarityMetric.COSINE.value());
 	}
 
 }