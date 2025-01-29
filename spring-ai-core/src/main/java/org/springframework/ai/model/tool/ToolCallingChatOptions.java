spring-projects/spring-ai

Advancing Tool Support - Part 3
* Introduced ToolCallingManager to manage the tool calling activities for resolving and executing tools. A default implementation is provided. It can be used to handle explicit tool execution on the client-side, superseding the previous FunctionCallingHelper class. It’s ready to be instrumented via Micrometer, and support exception handling when tool calls fail.
* Introduced ToolCallExceptionConverter to handle exceptions in tool calling, and provided a default implementation propagating the error message to the chat morel.
* Introduced ToolCallbackResolver to resolve ToolCallback instances. A default implementation is provided (DelegatingToolCallbackResolver), capable of delegating the resolution to a series of resolvers, including static resolution (StaticToolCallbackResolver) and dynamic resolution from the Spring context (SpringBeanToolCallbackResolver).
* Improved configuration in ToolCallingChatOptions to enable/disable the tool execution within a ChatModel (superseding the previous proxyToolCalls option).
* Added unit and integration tests to cover all the new use cases and existing functionality which was not covered by autotests (tool resolution from Spring context).
* Deprecated FunctionCallbackResolver, AbstractToolCallSupport, and FunctionCallingHelper.

Relates to gh-2049

Signed-off-by: Thomas Vitale <ThomasVitale@users.noreply.github.com>

spring-ai-core/src/main/java/org/springframework/ai/model/tool/ToolCallingChatOptions.java
@@ -16,11 +16,12 @@
 
 package org.springframework.ai.model.tool;
 
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.model.function.FunctionCallback;
 import org.springframework.ai.model.function.FunctionCallingOptions;
-import org.springframework.ai.tool.ToolCallback;
-import org.springframework.ai.tool.metadata.ToolMetadata;
 import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 import java.util.List;
 import java.util.Map;
@@ -35,20 +36,17 @@ import java.util.Set;
  */
 public interface ToolCallingChatOptions extends FunctionCallingOptions {
 
+	boolean DEFAULT_TOOL_EXECUTION_ENABLED = true;
+
 	/**
 	 * ToolCallbacks to be registered with the ChatModel.
 	 */
-	List<ToolCallback> getToolCallbacks();
+	List<FunctionCallback> getToolCallbacks();
 
 	/**
 	 * Set the ToolCallbacks to be registered with the ChatModel.
 	 */
-	void setToolCallbacks(List<ToolCallback> toolCallbacks);
-
-	/**
-	 * Set the ToolCallbacks to be registered with the ChatModel.
-	 */
-	void setToolCallbacks(ToolCallback... toolCallbacks);
+	void setToolCallbacks(List<FunctionCallback> toolCallbacks);
 
 	/**
 	 * Names of the tools to register with the ChatModel.
@@ -58,27 +56,20 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 	/**
 	 * Set the names of the tools to register with the ChatModel.
 	 */
-	void setTools(Set<String> tools);
+	void setTools(Set<String> toolNames);
 
 	/**
-	 * Set the names of the tools to register with the ChatModel.
-	 */
-	void setTools(String... tools);
-
-	/**
-	 * Whether the result of each tool call should be returned directly or passed back to
-	 * the model. It can be overridden for each {@link ToolCallback} instance via
-	 * {@link ToolMetadata#returnDirect()}.
+	 * Whether the {@link ChatModel} is responsible for executing the tools requested by
+	 * the model or if the tools should be executed directly by the caller.
 	 */
 	@Nullable
-	Boolean getToolCallReturnDirect();
+	Boolean isToolExecutionEnabled();
 
 	/**
-	 * Set whether the result of each tool call should be returned directly or passed back
-	 * to the model. It can be overridden for each {@link ToolCallback} instance via
-	 * {@link ToolMetadata#returnDirect()}.
+	 * Set whether the {@link ChatModel} is responsible for executing the tools requested
+	 * by the model or if the tools should be executed directly by the caller.
 	 */
-	void setToolCallReturnDirect(@Nullable Boolean toolCallReturnDirect);
+	void setToolExecutionEnabled(@Nullable Boolean toolExecutionEnabled);
 
 	/**
 	 * A builder to create a new {@link ToolCallingChatOptions} instance.
@@ -95,12 +86,12 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 		/**
 		 * ToolCallbacks to be registered with the ChatModel.
 		 */
-		Builder toolCallbacks(List<ToolCallback> functionCallbacks);
+		Builder toolCallbacks(List<FunctionCallback> functionCallbacks);
 
 		/**
 		 * ToolCallbacks to be registered with the ChatModel.
 		 */
-		Builder toolCallbacks(ToolCallback... functionCallbacks);
+		Builder toolCallbacks(FunctionCallback... functionCallbacks);
 
 		/**
 		 * Names of the tools to register with the ChatModel.
@@ -113,11 +104,10 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 		Builder tools(String... toolNames);
 
 		/**
-		 * Whether the result of each tool call should be returned directly or passed back
-		 * to the model. It can be overridden for each {@link ToolCallback} instance via
-		 * {@link ToolMetadata#returnDirect()}.
+		 * Whether the {@link ChatModel} is responsible for executing the tools requested
+		 * by the model or if the tools should be executed directly by the caller.
 		 */
-		Builder toolCallReturnDirect(@Nullable Boolean toolCallReturnDirect);
+		Builder toolExecutionEnabled(@Nullable Boolean toolExecutionEnabled);
 
 		// FunctionCallingOptions.Builder methods
 
@@ -144,7 +134,7 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 		Builder function(String function);
 
 		@Override
-		@Deprecated // Use toolCallReturnDirect() instead
+		@Deprecated // Use toolExecutionEnabled() instead
 		Builder proxyToolCalls(@Nullable Boolean proxyToolCalls);
 
 		// ChatOptions.Builder methods
@@ -176,6 +166,23 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 		@Override
 		ToolCallingChatOptions build();
 
+	}
+
+	static boolean isToolExecutionEnabled(ChatOptions chatOptions) {
+		Assert.notNull(chatOptions, "chatOptions cannot be null");
+		boolean toolExecutionEnabled;
+		if (chatOptions instanceof ToolCallingChatOptions toolCallingChatOptions
+				&& toolCallingChatOptions.isToolExecutionEnabled() != null) {
+			toolExecutionEnabled = Boolean.TRUE.equals(toolCallingChatOptions.isToolExecutionEnabled());
+		}
+		else if (chatOptions instanceof FunctionCallingOptions functionCallingOptions
+				&& functionCallingOptions.getProxyToolCalls() != null) {
+			toolExecutionEnabled = Boolean.TRUE.equals(!functionCallingOptions.getProxyToolCalls());
+		}
+		else {
+			toolExecutionEnabled = DEFAULT_TOOL_EXECUTION_ENABLED;
+		}
+		return toolExecutionEnabled;
 	}
 
 }

spring-ai-core/src/test/java/org/springframework/ai/model/tool/ToolCallingChatOptionsTests.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright 2023-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.model.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.model.function.FunctionCallingOptions;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * Unit tests for {@link ToolCallingChatOptions}.
+ *
+ * @author Thomas Vitale
+ */
+class ToolCallingChatOptionsTests {
+
+	@Test
+	void whenToolCallingChatOptionsAndExecutionEnabledTrue() {
+		ToolCallingChatOptions options = new DefaultToolCallingChatOptions();
+		options.setToolExecutionEnabled(true);
+		assertThat(ToolCallingChatOptions.isToolExecutionEnabled(options)).isTrue();
+	}
+
+	@Test
+	void whenToolCallingChatOptionsAndExecutionEnabledFalse() {
+		ToolCallingChatOptions options = new DefaultToolCallingChatOptions();
+		options.setToolExecutionEnabled(false);
+		assertThat(ToolCallingChatOptions.isToolExecutionEnabled(options)).isFalse();
+	}
+
+	@Test
+	void whenToolCallingChatOptionsAndExecutionEnabledDefault() {
+		ToolCallingChatOptions options = new DefaultToolCallingChatOptions();
+		assertThat(ToolCallingChatOptions.isToolExecutionEnabled(options)).isTrue();
+	}
+
+	@Test
+	void whenFunctionCallingOptionsAndExecutionEnabledTrue() {
+		FunctionCallingOptions options = FunctionCallingOptions.builder().build();
+		options.setProxyToolCalls(false);
+		assertThat(ToolCallingChatOptions.isToolExecutionEnabled(options)).isTrue();
+	}
+
+	@Test
+	void whenFunctionCallingOptionsAndExecutionEnabledFalse() {
+		FunctionCallingOptions options = FunctionCallingOptions.builder().build();
+		options.setProxyToolCalls(true);
+		assertThat(ToolCallingChatOptions.isToolExecutionEnabled(options)).isFalse();
+	}
+
+	@Test
+	void whenFunctionCallingOptionsAndExecutionEnabledDefault() {
+		FunctionCallingOptions options = FunctionCallingOptions.builder().build();
+		assertThat(ToolCallingChatOptions.isToolExecutionEnabled(options)).isTrue();
+	}
+
+}

intelimaster/inteli-ai-spring-framework

spring-ai-core/src/main/java/org/springframework/ai/model/tool/ToolCallingChatOptions.java
@@ -16,11 +16,12 @@
 
 package org.springframework.ai.model.tool;
 
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.model.function.FunctionCallback;
 import org.springframework.ai.model.function.FunctionCallingOptions;
-import org.springframework.ai.tool.ToolCallback;
-import org.springframework.ai.tool.metadata.ToolMetadata;
 import org.springframework.lang.Nullable;
+import org.springframework.util.Assert;
 
 import java.util.List;
 import java.util.Map;
@@ -35,20 +36,17 @@ import java.util.Set;
  */
 public interface ToolCallingChatOptions extends FunctionCallingOptions {
 
+	boolean DEFAULT_TOOL_EXECUTION_ENABLED = true;
+
 	/**
 	 * ToolCallbacks to be registered with the ChatModel.
 	 */
-	List<ToolCallback> getToolCallbacks();
+	List<FunctionCallback> getToolCallbacks();
 
 	/**
 	 * Set the ToolCallbacks to be registered with the ChatModel.
 	 */
-	void setToolCallbacks(List<ToolCallback> toolCallbacks);
-
-	/**
-	 * Set the ToolCallbacks to be registered with the ChatModel.
-	 */
-	void setToolCallbacks(ToolCallback... toolCallbacks);
+	void setToolCallbacks(List<FunctionCallback> toolCallbacks);
 
 	/**
 	 * Names of the tools to register with the ChatModel.
@@ -58,27 +56,20 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 	/**
 	 * Set the names of the tools to register with the ChatModel.
 	 */
-	void setTools(Set<String> tools);
+	void setTools(Set<String> toolNames);
 
 	/**
-	 * Set the names of the tools to register with the ChatModel.
-	 */
-	void setTools(String... tools);
-
-	/**
-	 * Whether the result of each tool call should be returned directly or passed back to
-	 * the model. It can be overridden for each {@link ToolCallback} instance via
-	 * {@link ToolMetadata#returnDirect()}.
+	 * Whether the {@link ChatModel} is responsible for executing the tools requested by
+	 * the model or if the tools should be executed directly by the caller.
 	 */
 	@Nullable
-	Boolean getToolCallReturnDirect();
+	Boolean isInternalToolExecutionEnabled();
 
 	/**
-	 * Set whether the result of each tool call should be returned directly or passed back
-	 * to the model. It can be overridden for each {@link ToolCallback} instance via
-	 * {@link ToolMetadata#returnDirect()}.
+	 * Set whether the {@link ChatModel} is responsible for executing the tools requested
+	 * by the model or if the tools should be executed directly by the caller.
 	 */
-	void setToolCallReturnDirect(@Nullable Boolean toolCallReturnDirect);
+	void setInternalToolExecutionEnabled(@Nullable Boolean internalToolExecutionEnabled);
 
 	/**
 	 * A builder to create a new {@link ToolCallingChatOptions} instance.
@@ -95,12 +86,12 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 		/**
 		 * ToolCallbacks to be registered with the ChatModel.
 		 */
-		Builder toolCallbacks(List<ToolCallback> functionCallbacks);
+		Builder toolCallbacks(List<FunctionCallback> functionCallbacks);
 
 		/**
 		 * ToolCallbacks to be registered with the ChatModel.
 		 */
-		Builder toolCallbacks(ToolCallback... functionCallbacks);
+		Builder toolCallbacks(FunctionCallback... functionCallbacks);
 
 		/**
 		 * Names of the tools to register with the ChatModel.
@@ -113,11 +104,10 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 		Builder tools(String... toolNames);
 
 		/**
-		 * Whether the result of each tool call should be returned directly or passed back
-		 * to the model. It can be overridden for each {@link ToolCallback} instance via
-		 * {@link ToolMetadata#returnDirect()}.
+		 * Whether the {@link ChatModel} is responsible for executing the tools requested
+		 * by the model or if the tools should be executed directly by the caller.
 		 */
-		Builder toolCallReturnDirect(@Nullable Boolean toolCallReturnDirect);
+		Builder internalToolExecutionEnabled(@Nullable Boolean internalToolExecutionEnabled);
 
 		// FunctionCallingOptions.Builder methods
 
@@ -144,7 +134,7 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 		Builder function(String function);
 
 		@Override
-		@Deprecated // Use toolCallReturnDirect() instead
+		@Deprecated // Use internalToolExecutionEnabled() instead
 		Builder proxyToolCalls(@Nullable Boolean proxyToolCalls);
 
 		// ChatOptions.Builder methods
@@ -176,6 +166,23 @@ public interface ToolCallingChatOptions extends FunctionCallingOptions {
 		@Override
 		ToolCallingChatOptions build();
 
+	}
+
+	static boolean isInternalToolExecutionEnabled(ChatOptions chatOptions) {
+		Assert.notNull(chatOptions, "chatOptions cannot be null");
+		boolean internalToolExecutionEnabled;
+		if (chatOptions instanceof ToolCallingChatOptions toolCallingChatOptions
+				&& toolCallingChatOptions.isInternalToolExecutionEnabled() != null) {
+			internalToolExecutionEnabled = Boolean.TRUE.equals(toolCallingChatOptions.isInternalToolExecutionEnabled());
+		}
+		else if (chatOptions instanceof FunctionCallingOptions functionCallingOptions
+				&& functionCallingOptions.getProxyToolCalls() != null) {
+			internalToolExecutionEnabled = Boolean.TRUE.equals(!functionCallingOptions.getProxyToolCalls());
+		}
+		else {
+			internalToolExecutionEnabled = DEFAULT_TOOL_EXECUTION_ENABLED;
+		}
+		return internalToolExecutionEnabled;
 	}
 
 }

spring-ai-core/src/test/java/org/springframework/ai/model/tool/ToolCallingChatOptionsTests.java
@@ -1,0 +1,70 @@
+/*
+ * Copyright 2023-2025 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.ai.model.tool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.ai.model.function.FunctionCallingOptions;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+/**
+ * Unit tests for {@link ToolCallingChatOptions}.
+ *
+ * @author Thomas Vitale
+ */
+class ToolCallingChatOptionsTests {
+
+	@Test
+	void whenToolCallingChatOptionsAndExecutionEnabledTrue() {
+		ToolCallingChatOptions options = new DefaultToolCallingChatOptions();
+		options.setInternalToolExecutionEnabled(true);
+		assertThat(ToolCallingChatOptions.isInternalToolExecutionEnabled(options)).isTrue();
+	}
+
+	@Test
+	void whenToolCallingChatOptionsAndExecutionEnabledFalse() {
+		ToolCallingChatOptions options = new DefaultToolCallingChatOptions();
+		options.setInternalToolExecutionEnabled(false);
+		assertThat(ToolCallingChatOptions.isInternalToolExecutionEnabled(options)).isFalse();
+	}
+
+	@Test
+	void whenToolCallingChatOptionsAndExecutionEnabledDefault() {
+		ToolCallingChatOptions options = new DefaultToolCallingChatOptions();
+		assertThat(ToolCallingChatOptions.isInternalToolExecutionEnabled(options)).isTrue();
+	}
+
+	@Test
+	void whenFunctionCallingOptionsAndExecutionEnabledTrue() {
+		FunctionCallingOptions options = FunctionCallingOptions.builder().build();
+		options.setProxyToolCalls(false);
+		assertThat(ToolCallingChatOptions.isInternalToolExecutionEnabled(options)).isTrue();
+	}
+
+	@Test
+	void whenFunctionCallingOptionsAndExecutionEnabledFalse() {
+		FunctionCallingOptions options = FunctionCallingOptions.builder().build();
+		options.setProxyToolCalls(true);
+		assertThat(ToolCallingChatOptions.isInternalToolExecutionEnabled(options)).isFalse();
+	}
+
+	@Test
+	void whenFunctionCallingOptionsAndExecutionEnabledDefault() {
+		FunctionCallingOptions options = FunctionCallingOptions.builder().build();
+		assertThat(ToolCallingChatOptions.isInternalToolExecutionEnabled(options)).isTrue();
+	}
+
+}